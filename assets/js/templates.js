af2951170331532f9b549bf635601a337d49a799
JeppeFH <jeppefinch@hotmail.dk>
2024-09-04 19:48:27 +0200

    update

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -5,10 +5,10 @@ export const productListTmpl = (product) =>
       <p class="title">${product.title}</p>
       <img src="${product.image}">
       <div>
-      <p>${product.price} kr.</p>
-      <button class="buy"><a href="page.html?id=${product.id}">Køb</a></button>
-      <button class="favBtn" id="${product.id}"><i class="fa-solid fa-heart"></i></button>
-  <button class="BasketBtn" id="${product.id}"><i class="fa-solid fa-basket-shopping"></i></button> 
+      <p>${product.price} kr.</p> 
+      <button class="buy"><a href="page.html?id=${product.id}">Køb</a></button>  
+      <button class="favBtn" id="${product.id}"><i class="fa-regular fa-bookmark"></i></button>
+     
   <div>
       </div>
   `;
@@ -17,7 +17,7 @@ export const productPageTmpl = (product) =>
   `
       <h2>${product.title}</h2>
       <p><i>Fra ${product.price} kr</i></p>
-      <img src="${product.image}${product.id}">
+      <img src="${product.image}">
       <p>${product.description}</p>
       <button><a href="index.html">Tilbage</a></button> 
       <button class="favBtn" id="${product.id}"><i class="fa-solid fa-heart"></i></button>
@@ -26,11 +26,16 @@ export const productPageTmpl = (product) =>
   `;
 
 export const favListTmpl = (product) =>
-  `
-      <p><b>${product.title}</b></p>
-      <p><i>${product.price} kr</i></p>
-      
-      <button><a href="page.html?id=${product.id}">Se produkt</a></button> 
-      <button class="removeBtn" id="${product.id}">Fjern fra favoritter <i class="fa-solid fa-heart"></i></button>
-      <button class="BasketBtn" id="${product.id}"><i class="fa-solid fa-basket-shopping"></i></button> 
+  `   
+      <div class="favorites">
+      <button class="removeBtn" id="${product.id}"><i class="fa-solid fa-xmark"></i></button>
+      <img src="${product.image}">
+      <div>
+      <label>
+      <p>${product.title}</p>
+      <p>${product.price} kr</p>
+      </label>
+      <button class="buy"><a href="page.html?id=${product.id}">Køb</a></button> 
+      </div>
+      </div>
       `;
